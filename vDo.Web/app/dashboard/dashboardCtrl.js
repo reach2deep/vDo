75401f0f5fd5877eeaad6c93aa07b4b3a46a94d0
reach2deep <reach2deep@gmail.com>
2016-08-20 13:41:20 +0800

    dashboard ui moved seperately

---
+++ vDo.Web/app/dashboard/dashboardCtrl.js
@@ -0,0 +1,5 @@
+﻿angular.module("MyApp")
+       .controller("dashboardCtrl", ['$scope', function ($scope) {
+           $scope.pageTitle = 'Dashboard';
+           $scope.message = 'This is the message from controller to view on page';
+       }]);
